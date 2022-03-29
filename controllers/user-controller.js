785afb52ec4ac71d4724d97810bd160e9ae75250
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-29 08:33:28 -0500

    user update controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -31,7 +31,18 @@ const userController = {
   },
 
   //update user
-  updateuser(req, res) {},
+  updateuser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 
   //delete User
   deleteUser(req, res) {},
