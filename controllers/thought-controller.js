f4c33dc39b1508ce7305cf771de8fe5bb3b44a7a
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-30 10:35:30 -0500

    reactions now can be added within thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -78,6 +78,40 @@ const thoughtController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  //create reactions
+  addreaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true }
+    )
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          res.status(404).json({ message: "No thought found with this id" });
+          return;
+        }
+        res.json(thoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //delete reactions
+  deletereaction({params}, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: {reactionId: params.reactionId} } },
+      { new: true }
+    )
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          res.status(404).json({ message: "No thought found with this id" });
+          return;
+        }
+        res.json(thoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -6,9 +6,20 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addreaction,
+  deletereaction,
 } = require("../../controllers/thought-controller");
 
+// /api/thoughts
 router.route("/").get(getAllthoughts).post(createThought);
+
+// /api/thoughts/:id
 router.route("/:id").get(getThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addreaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deletereaction);
 
 module.exports = router;
